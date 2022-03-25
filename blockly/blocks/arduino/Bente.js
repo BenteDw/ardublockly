b1e6ab740e7a50a4c7eb5e9d9e6896e2569d5569
Your Name <you@example.com>
2022-03-25 13:54:08 +0100

    ok

---
+++ blockly/blocks/arduino/Bente.js
@@ -0,0 +1,16 @@
+Blockly.Blocks['ledder_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Led Status");
+    this.appendDummyInput()
+        .appendField("LED")
+        .appendField(new Blockly.FieldDropdown([["1","LED1"], ["2","LED2"], ["3","LED3"], ["4","LED4"], ["5","LED5"], ["6","LED6"], ["allemaal","LEDALL"]]), "LED")
+        .appendField("status:")
+        .appendField(new Blockly.FieldDropdown([["aan","ON"], ["uit","OF"]]), "LEDSTATE");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(165);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blockly/generators/arduino/Bente2.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['ledder_block'] = function(block) {
+  var dropdown_led = block.getFieldValue('LED');
+  var dropdown_ledstate = block.getFieldValue('LEDSTATE');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'Bente\n';
+  return code;
+};
