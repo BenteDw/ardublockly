bb797e60d815dc5325df7d4050770425332e258f
Your Name <you@example.com>
2022-03-28 16:36:49 +0200

    io

--- blockly/blocks/arduino/beta.js
+++ blockly/blocks/arduino/beta.js
@@ -6,7 +6,7 @@ Blockly.Blocks['ledder_block'] = {
         .appendField("LED")
         .appendField(new Blockly.FieldDropdown([["1","LED1"], ["2","LED2"], ["3","LED3"], ["4","LED4"], ["5","LED5"], ["6","LED6"], ["allemaal","LEDALL"]]), "LED")
         .appendField("status:")
-        .appendField(new Blockly.FieldDropdown([["aan","ON"], ["uit","OF"]]), "LEDSTATE");
+        .appendField(new Blockly.FieldDropdown([["aan","true"], ["uit","false"]]), "LEDSTATE");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(165);
@@ -28,4 +28,35 @@ Blockly.Blocks['setup_block'] = {
  this.setTooltip("");
  this.setHelpUrl("");
   }
-};  
+};
+
+Blockly.Blocks['effect_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField("Effectenblok");
+    this.appendDummyInput()
+        .appendField("Effect:")
+        .appendField(new Blockly.FieldDropdown([["Draaien","ROTATE"], ["Rond","ROUND"], ["Flikkeren","BLINK"], ["Willekeurig aan","RANDOM_ON"], ["Willekeurig uit","RANDOM_OF"]]), "EFFECT");
+    this.appendDummyInput()
+        .appendField("Snelheid:")
+        .appendField(new Blockly.FieldDropdown([["Snel","FAST"], ["Medium","MEDIUM"], ["Traag","SLOW"]]), "SPEED");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("keuze van voorgeprogrammeerde effecten");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['create_effect'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("aanmaak effect");
+    this.appendStatementInput("FUNCS")
+        .setCheck("effectPointer");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
--- blockly/generators/arduino/beta.js
+++ blockly/generators/arduino/beta.js
@@ -7,11 +7,11 @@ Blockly.Arduino['ledder_block'] = function(block) {
   var dropdown_led = block.getFieldValue('LED');
   var dropdown_ledstate = block.getFieldValue('LEDSTATE');
   // TODO: Assemble JavaScript into code variable.
-  var code = dropdown_led + "\n" + dropdown_ledstate;
+  var code = "registers[0]=" + dropdown_ledstate + ";\nwritereg();";
   var pinSetupCode = 'pinMode(' + code + ', INPUT);\n';
-  Blockly.Arduino.addInclude("includeLed","ok boomer");
-  Blockly.Arduino.addDeclaration("declairLed","zoomer");
-  Blockly.Arduino.addSetup("setupvrLed","...setup");  
+  Blockly.Arduino.addInclude("includeLed","//includes:");
+  Blockly.Arduino.addDeclaration("declairLed","//declarations:");
+  Blockly.Arduino.addSetup("setupvrLed","//setups:");  
   return code;
 };  
 
@@ -23,18 +23,42 @@ Blockly.Arduino['setup_block'] = function(block) {
   Blockly.Arduino.addInclude("includeSetup","#include <avr/sleep.h> // Sleep Modes\n#include <avr/power.h> // Power management");
 
   Blockly.Arduino.addDeclaration("declairSetup","#define DATA 0 //data pin shift reg\n#define RCLK 1 //latch pin shift reg\n#define SRCLK 2 //clock pin shift reg\n#define BUTTON 4\n#define DELAYML 100\n#define LONGPRESS 1000");
+  
+  Blockly.Arduino.addDeclaration("declairVar","int count = 0;\nint mode = 0;\nbool pressed = false;\nboolean registers[6];\nbool sleep = false;\nlong pressTime = 0;\nlong duration;\nint ran;\nint prevRan;")
+  
+  Blockly.Arduino.addFunction("ISR","ISR (PCINT0_vect) {\n  count++;\n  duration = 1;\n  if (count == 1) pressTime = millis();\n  if(count >= 2) count = 0;\n  if(millis() - pressTime >= LONGPRESS) sleep = true;\n/*  else mode++;\n  if (mode > (sizeof(callEffect)>>1)) mode = 0;\n*/}");
 
+  Blockly.Arduino.addFunction("sleep","void goToSleep() {\n  memset(registers,false,sizeof(registers));\n  writereg();\n\n  set_sleep_mode(SLEEP_MODE_PWR_DOWN);\n  power_all_disable ();  // power off ADC, Timer 0 and 1, serial interface\n  sleep_enable();\n  sleep_cpu();\n  sleep_disable(); //power everything back on\n  power_all_enable();\n  sleep=false;\n  count=-1;\n  mode=0;\n}");
+  
+  Blockly.Arduino.addFunction("writereg","void writereg() {\n  digitalWrite(SRCLK,LOW);\n\n  for(int i=sizeof(registers)-1;i>=0;i--)\n  {\n    digitalWrite(RCLK,LOW);\n    digitalWrite(DATA,registers[i]);\n    digitalWrite(RCLK,HIGH);\n  }\n\n  digitalWrite(SRCLK,HIGH);\n}");
+  
+  if(checkbox_name==true)
+    { Blockly.Arduino.addSetup("setupSetup","pinMode(DATA,OUTPUT);\npinMode(RCLK,OUTPUT);\npinMode(SRCLK,OUTPUT);\npinMode(BUTTON,INPUT);\n\nmemset(registers,true,sizeof(registers));\nwritereg();\ndelay(DELAYML);\nmemset(registers,false,sizeof(registers));\nwritereg();\ndelay(DELAYML);")
+    }
+  else
+    {
+ Blockly.Arduino.addSetup("setupSetup","pinMode(DATA,OUTPUT);\npinMode(RCLK,OUTPUT);\npinMode(SRCLK,OUTPUT);\npinMode(BUTTON,INPUT);")
+    }
   
   
-
-
-
-
-
-  var code = "pinMode(DATA, OUTPUT);\npinMode(RCLK, OUTPUT);\npinMode(SRCLK, OUTPUT);\npinMode(BUTTON, INPUT);\n";
-  code += "ADCSRA = 0; // turn off ADC\nPCMSK  |= bit (PCINT4);  // want pin D4 / pin 3\nGIFR   |= bit (PCIF);    // clear any outstanding interrupts\nGIMSK  |= bit (PCIE);    // enable pin change interrupts\n";
-  if(checkbox_name==true) code += ('\nmemset(registers,sizeof(registers),true);\nwritereg();\ndelay(DELAYML);\nmemset(registers,sizeof(registers),false);\nwritereg();\ndelay(DELAYML);')
   
+  var code = "if (sleep) goToSleep();\n";
+
   return code;
 };  
 
+
+Blockly.Arduino['effect_block'] = function(block) {
+  var dropdown_effect = block.getFieldValue('EFFECT');
+  var dropdown_speed = block.getFieldValue('SPEED');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.Arduino['create_effect'] = function(block) {
+  var statements_name = Blockly.JavaScript.statementToCode(block, 'FUNCS');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
